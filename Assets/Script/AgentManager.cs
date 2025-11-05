8326a0136afd6acba649ab71fd79fa71b7ac1994
Fraharg <orlane.guillon4@gmail.com>
2025-11-05 15:33:55 +0100

    suite ia td

---
+++ Assets/Script/AgentManager.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+
+public class AgentManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _agent;
+    [SerializeField] private int _numberOfAgents = 1;
+
+    public List<GameObject> _agentsList = new List<GameObject>();
+
+
+    public float _agentsSpeed = 1f;
+
+    [SerializeField] private bool _isRandomTarget;
+
+    void Start()
+    {
+        if(_agent != null)
+        {
+            AgentsSpawn();
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnValidate()
+    {
+        ChangeAgentsValues();
+    }
+
+    void ChangeAgentsValues()
+    {
+        Debug.Log("changeSpeed");
+        foreach (GameObject i in _agentsList)
+        {
+            i.GetComponent<NavMeshAgent>().speed = _agentsSpeed;
+            Debug.Log(i.name + "is changing speed");
+            Debug.Log(i.GetComponent<NavMeshAgent>().speed);
+        }
+    }
+
+    void AgentsSpawn()
+    {
+        for (int i = 0; i < _numberOfAgents; i++)
+        {
+            Instantiate(_agent);
+            _agent.GetComponent<NavMeshAgent>().speed = _agentsSpeed;
+            _agent.GetComponent<PathFindingAI>()._isRandomTarget = _isRandomTarget;
+            _agentsList.Add(_agent);
+        }
+    }
+}
--- Assets/Script/PathFindingAI.cs
+++ Assets/Script/PathFindingAI.cs
@@ -1,17 +1,75 @@
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.AI;
 
 public class PathFindingAI : MonoBehaviour
 {
-    [SerializeField] private Transform _destination = null;
+    [SerializeField] private Transform _currentDestination = null;
     private NavMeshAgent _navMeshAgent = null;
+
+    public List<Transform> _targetList = new List<Transform>();
+
+    private int _targetIndex = 0;
+
+    private bool _endTarget = false;
+
+    public bool _isRandomTarget;
+
+
     void Start()
     {
         _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+        foreach (GameObject _target in GameObject.FindGameObjectsWithTag("Target"))
+        {
+            _targetList.Add(_target.GetComponent<Transform>());
+        }
+        _targetList.Reverse();
+
+        SetDestination();
     }
 
     void Update()
     {
-        _navMeshAgent.destination = _destination.position;
+        if (_endTarget == false)
+        {
+            _navMeshAgent.destination = _currentDestination.position;
+
+            if (Vector3.Distance(transform.position, _currentDestination.position) <= 1f)
+            {
+                Debug.Log("destination atteinte");
+                SetDestination();
+            }
+        }
+        else
+        {
+            _navMeshAgent.isStopped = true;
+        }
+    }
+
+    void SetDestination()
+    {
+        if (_targetIndex >= _targetList.Count)
+        {
+            _endTarget = true;
+        } else
+        {
+            if(_isRandomTarget == false)
+            {
+                //cas ordonné
+                _currentDestination = _targetList[_targetIndex];
+                _currentDestination.position = _currentDestination.position;
+                _targetIndex = _targetIndex + 1;
+            }
+            else
+            {
+                //cas aléatoire
+                int index = Random.Range(0, _targetList.Count);
+                _currentDestination = _targetList[index];
+                _targetList[index] = _targetList[_targetList.Count - 1];
+                _targetList.RemoveAt(_targetList.Count - 1);
+                _currentDestination.position = _currentDestination.position;
+            }
+        }
     }
 }
